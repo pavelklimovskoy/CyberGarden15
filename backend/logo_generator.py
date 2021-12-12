ecd6d22fbdf6b89bfaad365ad96bd5964edb8bfe
Evgeniy Ivanov <evgeniy.ivanov@starz.com>
2021-12-12 14:11:39 +0300

    Add P5 3D

--- backend/logo_generator.py
+++ backend/logo_generator.py
@@ -2,6 +2,9 @@ from image_search import download_image_from_query
 from style_transfer import StyleTransferer
 import sys
 import cv2
+import random
+
+true_queries = ['лев', 'ястреб', 'дерево', 'гора']
 
 class LogoGenerator:
     st = None
@@ -16,12 +19,19 @@ class LogoGenerator:
             styled_img_path = f'styled_{image_path}'
             cv2.imwrite(styled_img_path, styled_img)
             return styled_img_path
-        return None
+        else:
+            true_query = random.choice(true_queries)
+            image_path = download_image_from_query(true_query)
+            if image_path:
+                styled_img = self.st.transfer_random_style(image_path)
+                styled_img_path = f'styled_{image_path}'
+                cv2.imwrite(styled_img_path, styled_img)
+                return styled_img_path
+        return 'default.jpg'
 
 def __main__():
     lg = LogoGenerator()
-    print(lg.generate('бритва'))
-
+    print(lg.generate('ястреб'))
 
 if __name__ == '__main__':
     __main__()
